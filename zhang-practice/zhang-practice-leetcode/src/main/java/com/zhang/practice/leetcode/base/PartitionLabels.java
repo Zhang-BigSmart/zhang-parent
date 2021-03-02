c1572378309c30fd2750bce3481c193d2b6ac406
zzh <zzh@imlaidian.com>
2021-03-02 15:05:59 +0800

    leetcode：763-字符串分类

---
+++ zhang-practice/zhang-practice-leetcode/src/main/java/com/zhang/practice/leetcode/base/PartitionLabels.java
@@ -0,0 +1,50 @@
+package com.zhang.practice.leetcode.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : zzh
+ * create at:  2021/3/2
+ * @description:
+ * 字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：S = "ababcbacadefegdehijhklij"
+ * 输出：[9,7,8]
+ * 解释：
+ * 划分结果为 "ababcbaca", "defegde", "hijhklij"。
+ * 每个字母最多出现在一个片段中。
+ * 像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/partition-labels
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PartitionLabels {
+
+    public List<Integer> partitionLabels(String S) {
+        if (S == null || "".equals(S)) {
+            return null;
+        }
+        int[] last = new int[26];
+        int length = S.length();
+        for (int i = 0; i < length; i++) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+        int start = 0;
+        int end = 0;
+        List<Integer> partition = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[S.charAt(i) - 'a']);
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+}
