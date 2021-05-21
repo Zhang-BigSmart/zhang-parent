213fc3967e816b3a9f62a383a580f73a8ed5a2da
zheye <zheye.zhang@tuya.com>
2021-05-21 16:58:46 +0800

    update

--- zhang-practice/zhang-practice-java/src/main/java/com/zhang/practice/thread/thread/CallableTest.java
+++ zhang-practice/zhang-practice-java/src/main/java/com/zhang/practice/thread/thread/CallableTest.java
@@ -1,7 +1,61 @@
-package com.zhang.practice.thread.thread;/**
+package com.zhang.practice.thread.thread;
+
+import java.util.concurrent.*;
+
+/**
  * <p> TODO
- * 
- * @author 哲也（张梓濠 zheye.zhang@tuya.com）
+ *
+ * @author
  * @since 2021/4/24
- */public class CallableTest {
+ */
+public class CallableTest {
+
+    public static void main(String[] args) {
+
+        ExecutorService es = Executors.newSingleThreadExecutor();
+
+        CallableDemo callableDemo = new CallableDemo();
+
+        Future<Integer> future = es.submit(callableDemo);
+
+
+        //FutureTask
+
+        //关闭线程池
+        es.shutdown();
+        try {
+            Thread.sleep(2000);
+            System.out.println("主线程在执行其他任务");
+
+            if(future.get()!=null){
+                //输出获取到的结果
+                System.out.println("future.get()-->"+future.get());
+            }else{
+                //输出获取到的结果
+                System.out.println("future.get()未获取到结果");
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("主线程在执行完成");
+    }
+
+}
+
+class CallableDemo implements Callable<Integer> {
+
+    private int sum;
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Callable子线程开始计算啦！");
+        Thread.sleep(2000);
+
+        for(int i=0 ;i<5000;i++){
+            sum=sum+i;
+        }
+        System.out.println("Callable子线程计算结束！");
+        return sum;
+
+    }
 }
--- zhang-practice/zhang-practice-spring/src/main/java/com/zhang/practice/spring/web/TestController.java
+++ zhang-practice/zhang-practice-spring/src/main/java/com/zhang/practice/spring/web/TestController.java
@@ -3,15 +3,46 @@ package com.zhang.practice.spring.web;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
     @GetMapping("/user/{id}")
-    public Result<Mapper> get(@PathVariable Integer id) {
+    public Result<Mapper> get(@PathVariable Integer id, Model model) {
+
+        Map<String, Object> map = model.asMap();
+        System.out.println(map);
+
+        Result<Mapper> result = new Result<>();
+
+        Mapper mapper = new Mapper();
+        mapper.setId(id);
+        mapper.setName("name_" + id);
+        mapper.setFirst_name("first_name_" + id);
+        mapper.setUser_phone_no(id+1);
+        System.out.println("in!!!");
+
+        result.setResult(mapper);
+        result.setCode(400);
+        result.setMsg("success");
+        result.setSuccess(true);
+        result.setT(System.currentTimeMillis());
+
+        return result;
+    }
+
+
+    @GetMapping("/user/v2/{id}")
+    public Result<Mapper> get2(@PathVariable Integer id, @ModelAttribute("md2") HashMap map) {
+
+        System.out.println(map);
+
         Result<Mapper> result = new Result<>();
 
         Mapper mapper = new Mapper();
